2d6f64944d3c132928d09c372914f07ad08fd9a2
yby32400 <yby32400@gmail.com>
2026-01-18 08:27:19 +0900

    data.js 업데이트

--- data.js
+++ data.js
@@ -1,12 +1,14 @@
-/* data.js */
-// 1. 달력 일정
+/* ==========================================================
+   ★ 여기에 글을 쓰세요! (저장하면 사이트에 바로 반영됨)
+   ========================================================== */
+
+// 1. 달력 일정 (날짜: { 이미지, 링크 })
 const mySchedule = {
-    "1-24": { img: "https://images.unsplash.com/photo-1518199266791-5375a83190b7", link: "#" },
     "1-29": { img: "https://pbs.twimg.com/media/G-ubDR3bAAAeXV-?format=jpg&name=large", link: "https://drive.google.com" },
     "2-14": { img: "https://images.unsplash.com/photo-1518199266791-5375a83190b7", link: "#" }
 };
 
-// 2. 공지사항
+// 2. 공지사항 (Notice)
 const noticePosts = [
     {
         title: "아카이브 오픈 안내",
@@ -15,21 +17,21 @@ const noticePosts = [
     }
 ];
 
-// 3. 체키/사진 (다중 이미지 지원)
+// 3. 체키/사진 (Cheki) - images에 여러 장 넣기 가능
 const chekiPosts = [
     {
         title: "1월 29일 정산 (사진 예시)",
         date: "2026.01.29",
         tag: "CHEKI",
         images: [
-            "https://images.unsplash.com/photo-1595079676339-1534801ad6cf",
-            "https://images.unsplash.com/photo-1529156069898-49953e39b3ac"
+            "https://images.unsplash.com/photo-1595079676339-1534801ad6cf", // 첫번째
+            "https://images.unsplash.com/photo-1529156069898-49953e39b3ac"  // 두번째
         ],
         body: `오늘 카렌이랑 체키 찍었는데 너무 귀여웠음.`
     }
 ];
 
-// 4. 미디어
+// 4. 미디어 (Media)
 const mediaPosts = [
     {
         title: "페르소나5 플레이 영상",
