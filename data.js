71e60ec3e38388a4133652a21e0b21bf5f392f95
yby32400 <yby32400@gmail.com>
2026-01-18 12:52:33 +0900

    data.js 업데이트

--- data.js
+++ data.js
@@ -1,35 +1,39 @@
 /* ==========================================================
-   ★ 여기에 글을 쓰세요! (저장하면 사이트에 바로 반영됨)
+   ★ 데이터 관리 파일 (오류 수정본)
    ========================================================== */
 
 // 1. 달력 일정 (날짜: { 이미지, 링크 })
 const mySchedule = {
-    "1-29": { img: "https://pbs.twimg.com/media/G-ubDR3bAAAeXV-?format=jpg&name=large", link: "https://drive.google.com" },
-    "2-14": { img: "https://images.unsplash.com/photo-1518199266791-5375a83190b7", link: "#" }
+    "1-29": { 
+        img: "https://pbs.twimg.com/media/G-ubDR3bAAAeXV-?format=jpg&name=large", 
+        link: "https://drive.google.com" 
+    },
+    "2-14": { 
+        img: "https://images.unsplash.com/photo-1518199266791-5375a83190b7", 
+        link: "#" 
+    }
 };
 
 // 2. 공지사항 (Notice)
-
 const noticePosts = [
     {
         title: "아카이브 오픈 안내",
         date: "2026.01.18",
-        body: "여기에 꼭 따옴표를 앞뒤로 붙여주세요!" 
+        body: "옵시드 카렌 아카이브가 정식으로 오픈되었습니다!" 
     }
 ];
-/* ... 나머지 코드는 그대로 붙여넣으세요 ... */
 
-// 3. 체키/사진 (Cheki) - images에 여러 장 넣기 가능
+// 3. 체키/사진 (Cheki)
 const chekiPosts = [
     {
         title: "1월 29일 정산 (사진 예시)",
         date: "2026.01.29",
         tag: "CHEKI",
         images: [
-            "https://images.unsplash.com/photo-1595079676339-1534801ad6cf", // 첫번째
-            "https://images.unsplash.com/photo-1529156069898-49953e39b3ac"  // 두번째
+            "https://images.unsplash.com/photo-1595079676339-1534801ad6cf",
+            "https://images.unsplash.com/photo-1529156069898-49953e39b3ac"
         ],
-        body: `오늘 카렌이랑 체키 찍었는데 너무 귀여웠음.`
+        body: "오늘 카렌이랑 체키 찍었는데 너무 귀여웠음."
     }
 ];
 
